42aa9e29770f405779e592994155bb03076b8c7b
Nick Smith <nick.smith@cern.ch>
2020-10-28 06:49:23 +0100

    Make a schema for everyone, ditch avro

--- runall.sh
+++ runall.sh
@@ -1,13 +1,24 @@
-hdfs dfs -rm -R hdfs://analytix/user/ncsmith/working_set_cmssw
-spark-submit --packages com.databricks:spark-avro_2.11:4.0.0 generate_working_set.py --source cmssw --out hdfs://analytix/user/ncsmith/working_set_cmssw
-hdfs dfs -cp hdfs://analytix/user/ncsmith/working_set_cmssw root://eosuser/eos/user/n/ncsmith/hadoop-transfer/
+#!/usr/bin/env bash
+echo $HOSTNAME|grep -q 'ithdp-client' || return
+source hadoop-setconf.sh analytix 3.2 spark3
 
-hdfs dfs -rm -R hdfs://analytix/user/ncsmith/working_set_fwjr
-spark-submit --packages com.databricks:spark-avro_2.11:4.0.0 generate_working_set.py --source fwjr --out hdfs://analytix/user/ncsmith/working_set_fwjr
-hdfs dfs -cp hdfs://analytix/user/ncsmith/working_set_fwjr root://eosuser/eos/user/n/ncsmith/hadoop-transfer/
+dates="2020/*/*"
+outprefix="root://eosuser/eos/user/n/ncsmith/hadoop-transfer/" 
 
-hdfs dfs -rm -R hdfs://analytix/user/ncsmith/working_set_classads
-spark-submit --packages com.databricks:spark-avro_2.11:4.0.0 generate_working_set.py --source classads --out hdfs://analytix/user/ncsmith/working_set_classads
-hdfs dfs -cp hdfs://analytix/user/ncsmith/working_set_classads root://eosuser/eos/user/n/ncsmith/hadoop-transfer/
+hdfs dfs -rm -R hdfs://analytix/user/$USER/working_set_cmssw
+spark-submit generate_working_set.py --source cmssw --out hdfs://analytix/user/$USER/working_set_cmssw --dates $dates
+hdfs dfs -cp hdfs://analytix/user/$USER/working_set_cmssw $outprefix
 
-hdfs dfs -cp hdfs://analytix/project/awg/cms/CMS_DBS3_PROD_GLOBAL/current/BLOCKS/part-m-00000 root://eosuser/eos/user/n/ncsmith/hadoop-transfer/dbs_blocks.csv
+hdfs dfs -rm -R hdfs://analytix/user/$USER/working_set_fwjr
+spark-submit generate_working_set.py --source fwjr --out hdfs://analytix/user/$USER/working_set_fwjr --dates $dates
+hdfs dfs -cp hdfs://analytix/user/$USER/working_set_fwjr $outprefix
+
+hdfs dfs -rm -R hdfs://analytix/user/$USER/working_set_classads
+spark-submit generate_working_set.py --source classads --out hdfs://analytix/user/$USER/working_set_classads --dates $dates
+hdfs dfs -cp hdfs://analytix/user/$USER/working_set_classads $outprefix
+
+hdfs dfs -rm -R hdfs://analytix/user/$USER/working_set_xrootd
+spark-submit generate_working_set.py --source xrootd --out hdfs://analytix/user/$USER/working_set_xrootd --dates $dates
+hdfs dfs -cp hdfs://analytix/user/$USER/working_set_xrootd $outprefix
+
+hdfs dfs -cp hdfs://analytix/project/awg/cms/CMS_DBS3_PROD_GLOBAL/current/BLOCKS/part-m-00000 ${outprefix}dbs_blocks.csv
